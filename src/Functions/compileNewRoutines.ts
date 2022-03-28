2404228eac7b1ea2d8542588297b94b21c84e7e0
alexraffy <92450474+alexraffy@users.noreply.github.com>
2022-03-29 01:43:53 +0200

    fixed compileNewRoutines

--- src/Functions/compileNewRoutines.ts
+++ src/Functions/compileNewRoutines.ts
@@ -14,14 +14,18 @@ export function compileNewRoutines(db: SKSQL) {
     let cursor = readFirst(tblRoutines, def);
     let colNameDef = def.columns.find((t) => { return t.name === "name";});
     let colDefinitionDef = def.columns.find((t) => { return t.name === "definition";});
+    let functions : {name: string; fnDef: string}[] = [];
+
     while (!cursorEOF(cursor)) {
         let fr = new DataView(tblRoutines.data.blocks[cursor.blockIndex], cursor.offset, cursor.rowLength + 5);
         const name = readValue(tblRoutines, def, colNameDef, fr, 5) as string;
         const fnDef = readValue(tblRoutines, def, colDefinitionDef, fr, 5) as string;
-        let sql = new SQLStatement(db, fnDef, false);
-        sql.run();
-
+        functions.push({name: name, fnDef: fnDef});
         cursor = readNext(tblRoutines, def, cursor);
+    }
+    for (let i = 0; i < functions.length; i++) {
+        let sql = new SQLStatement(db, functions[i].fnDef, false);
+        sql.run();
     }
 }
 
